8597a49d1964cd0c8f3e6efaad3d20c00d911876
Eric Jobe <ericjobe@Erics-MacBook-Pro.local>
2024-04-25 11:03:24 -0500

    added dynamic pages from json file

---
+++ src/assets/apache.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-apache',
+  standalone: true,
+  imports: [],
+  templateUrl: './apache.component.html',
+  styleUrl: './apache.component.css'
+})
+export class ApacheComponent {
+
+}
---
+++ src/assets/privily.vue
@@ -0,0 +1,20 @@
+<script setup>
+    import { ref } from 'vue'
+    import TodoItem from './TodoItem.vue'
+
+    const groceryList = ref([
+        { id: 0, text: 'Vegetables' },
+        { id: 1, text: 'Cheese' },
+        { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ])
+</script>
+
+<template>
+  <ol>
+    <TodoItem
+      v-for="item in groceryList"
+      :todo="item"
+      :key="item.id"
+    ></TodoItem>
+  </ol>
+</template>
